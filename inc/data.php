d70308cf71abe214e0a63ea1e18d3767cd54a43a
Russell Kahmann <russellkahmann@Russells-MBP.cinci.rr.com>
2017-05-06 19:15:10 -0400

    first commit

---
+++ inc/data.php
@@ -0,0 +1,173 @@
+<?php
+$catalog = [];
+//Books
+$catalog[101] = [
+	"title" => "The Magic of Recluse",
+	"img" => "img/media/recluse.jpg",
+    "genre" => "Fantasy",
+    "format" => "Paperback",
+    "year" => 1992,
+    "category" => "Books",
+    "authors" => [
+        "L.E. Modesitt Jr."
+    ],
+    "publisher" => "Tor Fantasy",
+    "isbn" => '978-0812505184'
+];
+$catalog[102] = [
+    "title" => "Dragons of Autumn Twilight",
+    "img" => "img/media/autumn_twilight.jpg",
+    "genre" => "Fantasy",
+    "format" => "Paperback",
+    "year" => 2000,
+    "category" => "Books",
+    "authors" => [
+        "Margaret Weiss",
+				"Tracy Hickman"
+    ],
+    "publisher" => "Wizards of the Coast; Reissue edition",
+    "isbn" => '978-0786915743'
+];
+$catalog[103] = [
+    "title" => "Elements of Sonata Theory",
+    "img" => "img/media/sonata_theory.jpg",
+    "genre" => "Music",
+    "format" => "Hardcover",
+    "year" => 2006,
+    "category" => "Books",
+    "authors" => [
+        "James Hepokoski",
+				"Warren Darcy"
+    ],
+    "publisher" => "Oxford University Press",
+    "isbn" => '978-0195146400'
+];
+$catalog[104] = [
+    "title" => "Music in Transition: A Study of Tonal Expansion and Antonality 1900-1920",
+    "img" => "img/media/music_transition.jpg",
+    "genre" => "Music",
+    "format" => "Paperback",
+    "year" => 1995,
+    "category" => "Books",
+    "authors" => [
+        "Jim Samson"
+    ],
+    "publisher" => "Dent",
+    "isbn" => '978-046-61502'
+];
+//Movies
+$catalog[201] = [
+    "title" => "Moana",
+    "img" => "img/media/moana.jpg",
+    "genre" => "Animation",
+    "format" => "Blu-ray",
+    "year" => 2017,
+    "category" => "Movies",
+    "director" => [
+			"Ron Clements",
+			"John Musker",
+			"Don Hall",
+			"Chris Williams"
+		],
+    "writers" => [
+        "Jared Bush",
+    ],
+    "stars" => [
+        "Auli Cravalho",
+        "Dwayne Johnson",
+        "Rachel House,",
+        "Temuera Morrison",
+				"Jemaine Clement"
+    ]
+];
+$catalog[202] = [
+    "title" => "Cowboy Bebop: The Movie",
+    "img" => "img/media/bebop.jpg",
+    "genre" => "Animation",
+    "format" => "Blu-ray",
+    "year" => 2011,
+    "category" => "Movies",
+    "director" => "Shinichiro Watanabe",
+    "writers" => [
+        "Keiko Nobumoto"
+    ],
+    "stars" => [
+        "Steve Blum",
+        "Beau Billingslea",
+    ]
+];
+$catalog[203] = [
+    "title" => "The Boondock Saints",
+    "img" => "img/media/boondock.jpg",
+    "genre" => "Action",
+    "format" => "Blu-ray",
+    "year" => 2009,
+    "category" => "Movies",
+    "director" => "Troy Duffy",
+    "writers" => [
+        "Troy Duffy"
+    ],
+    "stars" => [
+        "Willem Dafoe",
+        "Sean Patrick Flanery",
+        "Norman Reedus",
+        "David Della Rocco",
+        "Billy Connolly",
+    ]
+];
+$catalog[204] = [
+    "title" => "Donnie Darko",
+    "img" => "img/media/darko.jpg",
+    "genre" => "Sci-Fi",
+    "format" => "Blu-ray",
+    "year" => 2009,
+    "category" => "Movies",
+    "director" => "Richard Kelly",
+    "writers" => [
+        "Richard Kelly"
+    ],
+    "stars" => [
+        "Jake Gyllenhaal",
+        "Jena Malone",
+        "Mary McDonnell",
+        "Holmes Osborne",
+        "Maggie Gyllenhaal",
+    ]
+];
+//Music
+$catalog[301] = [
+    "title" => "Dethalbum II",
+    "img" => "img/media/dethalbum2.jpg",
+    "genre" => "Metal",
+    "format" => "CD",
+    "year" => 2009,
+    "category" => "Music",
+    "artist" => "Dethklok"
+];
+$catalog[302] = [
+    "title" => "Songbook",
+    "img" => "img/media/songbook.jpg",
+    "genre" => "Jazz",
+    "format" => "CD",
+    "year" => 1997,
+    "category" => "Music",
+    "artist" => "Kenny Garrett"
+];
+$catalog[303] = [
+    "title" => "Red Queen to Gryphon Three",
+    "img" => "img/media/gryphon.jpg",
+    "genre" => "Rock",
+    "format" => "CD",
+    "year" => 2016,
+    "category" => "Music",
+    "artist" => "Gryphon"
+];
+$catalog[304] = [
+    "title" => "Hot Show",
+    "img" => "img/media/prozzak.jpg",
+    "genre" => "Dance",
+    "format" => "MP3",
+    "year" => 1999,
+    "category" => "Music",
+    "artist" => "Prozzak"
+];
---
+++ inc/footer.php
@@ -0,0 +1,20 @@
+</div><!-- end content -->
+
+	<div class="footer">
+
+		<div class="wrapper">
+
+			<ul>
+				<li><a href="https://twitter.com/RogueMC_Designs">Twitter</a></li>
+				<li><a href="http://facebook.com/roguemiddlechild">Facebook</a></li>
+			</ul>
+
+			<p>&copy;<?php echo date("Y"); ?> My Media Recommendations</p>
+			<p>Altered from a<a href="http://www.treehouse.com">TeamTreehouse</a>course.</p>
+
+		</div>
+
+	</div>
+
+</body>
+</html>
